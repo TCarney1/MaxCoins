2b6b34ca28ac6e8c31bdb54b3726bb26a17c4b17
Thomas <thomas.carney@griffithuni.edu.au>
2020-03-26 12:04:52 +1000

    complete

---
+++ cmake-build-debug/CMakeFiles/cpuTimerTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpuTimerTest.dir/main.cpp.obj"
+  "cpuTimerTest.exe"
+  "cpuTimerTest.exe.manifest"
+  "cpuTimerTest.lib"
+  "cpuTimerTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpuTimerTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
